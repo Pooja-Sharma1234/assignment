f003b63244d990c670a9ee0b8d77d70661f150d4
pooja <pooja8295sharma@gmail.com>
2024-02-26 14:08:08 +0530

    fixed home screen

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,5 +1,12 @@
 import {gql, useQuery} from '@apollo/client';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 const GET_LIST = gql`
   query ExampleQuery {
     countries {
@@ -17,19 +24,43 @@ const GET_LIST = gql`
 export const Home = () => {
   const {loading, error, data} = useQuery(GET_LIST);
 
-  if (loading) return <Text>Loading...</Text>;
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
   if (error) return <Text>Error :</Text>;
 
   return (
-    <ScrollView>
-      {data.countries.map((continent: any, index: any) => {
-        return (
-          <View key={index}>
-            <Text>{continent.code}</Text>
-            <Text>{continent.emoji}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <View style={{flex: 1}}>
+      <View
+        style={{justifyContent: 'center', alignItems: 'center', padding: 10}}>
+        <Text style={{fontSize: 30}}>Countries</Text>
+      </View>
+      <FlatList
+        data={data?.countries}
+        renderItem={({item, index}) => {
+          return (
+            <View
+              key={index}
+              style={{
+                margin: 15,
+                padding: 20,
+                elevation: 2,
+                backgroundColor: 'white',
+              }}>
+              <Text>Name:{item.name}</Text>
+              <Text>Code:{item.code}</Text>
+              <Text>emoji:{item.emoji}</Text>
+              <Text>phone:{item.phone}</Text>
+              <Text>Currency:{item.currency}</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 };
